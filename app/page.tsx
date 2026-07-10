6ba4096de457fcc2541f9207a2a8c00b36c6b721
habib-676 <rahman.habibur2421k@gmail.com>
2026-07-11 01:21:46 +0600

    feat: implement BookCard component and add sampleBooks for library display

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,22 @@
+import BookCard from "@/components/BookCard";
 import HeroSection from "@/components/HeroSection";
+import { sampleBooks } from "@/lib/constants";
 
 export default function Home() {
   return (
-    <div>
+    <div className="wrapper ">
       <HeroSection />
+      <div className="library-books-grid">
+        {sampleBooks.map((book) => (
+          <BookCard
+            key={book._id}
+            title={book.title}
+            author={book.author}
+            coverURL={book.coverURL}
+            slug={book.slug}
+          />
+        ))}
+      </div>
     </div>
   );
 }
